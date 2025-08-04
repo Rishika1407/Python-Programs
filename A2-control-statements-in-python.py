e4b255d7b2693b9e360c26d3b95238419ceb3d13
Rishika1407 <rishikaagrawal985@gmail.com>
2025-08-04 23:18:21 +0530

    Add files via upload
    Task 1 : Program for finding even/odd number.
    Task2 : Program for sum of 1 -50.

---
+++ A2-control-statements-in-python.py
@@ -0,0 +1,25 @@
+#A2-control-statements-in-python-
+#Task 1 and Task 2
+#task 1 question 1
+
+num = int(input("Enter the number: "))
+
+if num % 2 == 0:
+    print("The integer is even.")
+else:
+    print("The integer is odd.")
+
+
+#task 2 Q1
+
+total = 0
+
+for num in range(1, 51):  
+    total += num          
+
+print("The sum of integers from 1 to 50 is:", total)
+
+
+
+
+          
